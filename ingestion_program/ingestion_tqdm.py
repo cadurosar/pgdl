50ea6ad798d85f79f0691f8d74283d5f1e376670
Carlos <cadurosar@gmail.com>
2020-09-09 21:44:22 +0200

    fix merge error

--- ingestion_program/ingestion_tqdm.py
+++ ingestion_program/ingestion_tqdm.py
@@ -95,7 +95,6 @@ logging.getLogger("tensorflow").setLevel(logging.ERROR)
 import tensorflow as tf
 # enable tf 2.0 behavior
 tf.compat.v1.enable_v2_behavior()
-<<<<<<< Updated upstream
 # tf.logging.set_verbosity(tf.logging.ERROR)
 
 filter_filenames = [".ds_store", ".DS_Store", "__MACOSX"]
